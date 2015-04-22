ca8989261e3ac0ce19f6ec035a61f9777db7df26
iainredmill <iainredmill@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-22 13:04:40 +0000

    tagging v 1.1 retry
    git-svn-id: https://plugins.svn.wordpress.org/on-screen-keyboard/tags/1.1@1142132 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/onscreenkeyboard.php
@@ -0,0 +1,42 @@
+<?php
+/*
+ * Plugin Name: Onscreen Keyboard
+ * Plugin URI: http://not-applicable
+ * Description: This plugin adds an onscreen keyboard powered by Javascript/JQuery, ported from github by Iain Redmill, now working within WordPress, allowing mouse based input or input when working from a touch screen device.  Keyboard settings etc can be modified by editing the file.  To add a keyboard, enter <b>[oskb]</b> within your page where you wish it to appear.
+ * Version: 1.1
+ * Author: Ported by Iain Redmill
+ * Author URI: http://URI_Of_The_Plugin_Author
+ * Network: true
+ * License: GPL2
+ */
+
+
+function onscreenkeyboard_the_content( $content ) {
+   return str_replace("[oskb]",'<div id="virtualKeyboard"></div>',$content);
+}
+
+add_filter( 'the_content', 'onscreenkeyboard_the_content' );
+/*  
+// This just echoes the chosen line, we'll position it later
+function onscreenkeyboard() {
+	echo "testing 123 ";
+	
+	
+// Now we set that function up to execute when the admin_notices action is called
+add_action( 'admin_notices', 'onscreenkeyboard' );
+*/
+
+// We need some CSS to position the paragraph
+function onscreenkeyboard_css() {
+	// This makes sure that the positioning is also good for right-to-left languages
+	wp_register_style('onscreenkeyboardcss', plugins_url( 'on-screen-keyboard/css/jskeyboard.css'),array(),'1.1','screen');
+	wp_enqueue_style( 'onscreenkeyboardcss' );	
+	wp_register_script('onscreenkeyboard', plugins_url( 'on-screen-keyboard/js/jskeyboard.js'), array('jquery'),'1.1', true);
+	wp_register_script('onscreenkeyboard.main', plugins_url( 'on-screen-keyboard/js/jskeyboard.main.js'), array('jquery'),'1.1', true);
+	wp_enqueue_script('onscreenkeyboard');
+	wp_enqueue_script('onscreenkeyboard.main');
+}
+
+add_action( 'wp_enqueue_scripts', 'onscreenkeyboard_css' );
+  
+?>
---
+++ trunk/readme.txt
@@ -0,0 +1,49 @@
+=== Plugin Name ===
+Contributors: iainredmill
+Donate link: http://onscreenkeyboard.co.nf/
+Tags: comments, , onscreenkeyboard, onscreen, screen, keyboard, data-entry, mouse, touchscreen
+Requires at least: 3.0.1
+Tested up to: 4.1.1
+Stable tag: 1.1
+License: GPLv2 or later
+License URI: http://directory.fsf.org/wiki/License:Expat
+
+
+This plugin adds an onscreen keyboard powered by Javascript/JQuery to your page on demand.
+
+== Description ==
+
+This plugin adds an onscreen keyboard powered by Javascript/JQuery, ported from Samuel Deering's work
+on github with his permission under the above licence by Iain Redmill, now working within WordPress, 
+allowing mouse based input or input when working from a touch screen device.  Keyboard settings etc
+can be modified by editing the file.  To add a keyboard, enter [oskb] within your page where you wish 
+it to appear.
+
+
+== Installation ==
+
+1. Upload 'onscreenkeyboard.zip' and extract to the `/wp-content/plugins/on-screen-keyboard` directory
+or use the wordpress plugin installer
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Place `[oskb]` in your page at the point you wish an keyboard to appear
+
+== Frequently Asked Questions ==
+
+= I've installed v1.0 but the keyboard fails to pop up =
+
+There is a bug in v1.0, preventing the plugin triggering, please upgrade to v1.1, where this is fixed.
+
+== Screenshots ==
+
+See http://onscreenkeyboard.co.nf/ for working demo
+
+== Upgrade Notice ==
+
+Version 1.1:  Installer bug fixed, which prevented plugin working.
+
+== Changelog ==
+
+= 1.1 =
+* Installer bug fixed, which prevented plugin working.
+= 1.0 =
+* Plugin Launched
